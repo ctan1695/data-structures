8ee9e1f6416945cae7332382e1f8bff4ffcbcd11
Chloe Tan <chloe.tan95@gmail.com>
2020-10-27 22:35:03 -0400

    Partially finished Tree

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -3,7 +3,8 @@ var Tree = function(value) {
   newTree.value = value;
 
   // your code here
-  newTree.children = null;  // fix me
+  _.extend(newTree, treeMethods);
+  newTree.children = [];  // fix me
 
   return newTree;
 };
@@ -11,9 +12,22 @@ var Tree = function(value) {
 var treeMethods = {};
 
 treeMethods.addChild = function(value) {
+  this.children.push(Tree(value));
 };
 
 treeMethods.contains = function(target) {
+  var parentsTree = this;
+  var findTarget = function(target, currentTree) {
+    if (currentTree.value === target) {
+      return true;
+    } else {
+      for (var i = 0; i < currentTree.children.length; i++) {
+        return findTarget(target, currentTree.children[i]);
+      }
+    }
+    return false;
+  };
+  findTarget(target, parentsTree);
 };
 
 
