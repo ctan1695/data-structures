bc187463fc9eafa082c58a8dace9489c2e7207a0
Chloe Tan <chloe.tan95@gmail.com>
2020-10-27 21:26:34 -0400

    Complete Linked List

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,14 +4,52 @@ var LinkedList = function() {
   list.tail = null;
 
   list.addToTail = function(value) {
+    if (!list.tail) {
+      list.head = new Node(value);
+      list.tail = new Node(value);
+    } else {
+      var oldTail = list.tail;
+      list.tail = new Node(value);
+      oldTail.next = list.tail;
+    }
   };
 
   list.removeHead = function() {
+    //if list is empty
+    if (!list.head) {
+      return null;
+    } else {
+      //if list is not empty
+      var removedHead = list.head;
+      //if only one node left (head and tail are equal to same node)
+      if (list.head === list.tail) {
+        list.head = null;
+        list.tail = null;
+      } else if (list.head.next === null) {
+        list.head = list.tail;
+      } else {
+        list.head = list.head.next;
+      }
+      return removedHead.value;
+    }
   };
 
   list.contains = function(target) {
+    var currentNode = list.head;
+    while (currentNode) {
+      if (currentNode.value === target) {
+        return true;
+      }
+      if (currentNode.next !== null) {
+        currentNode = currentNode.next;
+      } else if (currentNode !== list.tail) {
+        currentNode = list.tail;
+      } else {
+        break;
+      }
+    }
+    return false;
   };
-
   return list;
 };
 
