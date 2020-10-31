d1b73f307174db4905e45c50c8eb9d40ec3afe4f
Chloe Tan <chloe.tan95@gmail.com>
2020-10-31 12:54:36 -0400

    Complete Set

--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -1,20 +1,31 @@
 var Set = function() {
   var set = Object.create(setPrototype);
-  set._storage = null; // fix me
+  set._storage = [];
   return set;
 };
 
 var setPrototype = {};
 
 setPrototype.add = function(item) {
+  this._storage.push(item);
 };
 
 setPrototype.contains = function(item) {
+  if (this._storage.includes(item)) {
+    return true;
+  } else {
+    return false;
+  }
 };
 
 setPrototype.remove = function(item) {
+  var index = this._storage.indexOf(item);
+  this._storage.splice(index, 1);
 };
 
 /*
  * Complexity: What is the time complexity of the above functions?
+ .add: constant
+ .contains: linear
+ .remove: linear
  */
