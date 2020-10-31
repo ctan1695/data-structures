25e8049a1168244bd74eaca928bb71c7f27fee1c
Chloe Tan <chloe.tan95@gmail.com>
2020-10-31 14:43:28 -0400

    Almost completed HashTable

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -6,21 +6,55 @@ var HashTable = function() {
 };
 
 HashTable.prototype.insert = function(k, v) {
+
   var index = getIndexBelowMaxForKey(k, this._limit);
+  var nestedStorage = [];
+  var nestedTuple = [];
+  nestedTuple.push(k);
+  nestedTuple.push(v);
+
+  // nestedStorage.push(v);
+  if (this._storage[index]) {
+    nestedStorage = this._storage[index];
+    for (var i = 0; i < nestedStorage.length; i++) {
+      if (nestedStorage[i][0] === k) {
+        this._storage[index].splice(i, 1);
+      }
+    }
+  }
+
+  nestedStorage.push(nestedTuple);
+  this._storage[index] = nestedStorage;
+
 };
 
 HashTable.prototype.retrieve = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  var getValue = function(input, arr) {
+    for (var i = 0; i < arr.length; i++) {
+      if (arr[i][0] === input) {
+        return arr[i][1];
+      }
+    }
+  };
+
+  for (var key in this._storage) {
+    if (typeof this._storage[key] !== 'function') {
+      return getValue(k, this._storage[key]);
+    }
+  }
 };
 
 HashTable.prototype.remove = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  delete this._storage[index];
 };
-
-
 
 /*
  * Complexity: What is the time complexity of the above functions?
+ insert : linear
+ retrieve: linear
+ remove: linear
  */
 
 
